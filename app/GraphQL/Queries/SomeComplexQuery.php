292f7f6815c6a8b40e6fcc4ab06bbe6737441dc1
sujeet kumar <kumarsujeet5565@gmail.com>
2023-04-03 17:03:02 +0530

    curd_operation_and _make_search_function

---
+++ app/GraphQL/Queries/SomeComplexQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\GraphQL\Queries;
+use App\Models\Product;
+
+final class SomeComplexQuery
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // some Complex Query
+
+
+        return Product::where('name', 'like', '%'.$args['search'].'%')->get();
+    }
+}
